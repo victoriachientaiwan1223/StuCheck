d97e56f6d232e40049143f71674c98cac57180db
victoriachientaiwan1223 <victoriachien.taiwan@gmail.com>
2026-09-19 23:50:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 // 部署新版 Apps Script 後，將 /exec 網址貼在 apiUrl 引號內。
 window.STUDENT_LOOKUP_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyenLkKjYlwF28PDlgGD8kc2kO2y_RcPMJQYe44KBiopC0tp9hj7czIIhwYzN10G2yv/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycby8sE1pcPDcNoMEqonwaZt6pwN87LmD4eLf4DoZ6wqm8BZok1PW6sV_xjJFdjJx-cM/exec",
 };
